866bb158268225595d872c4a6b4514a21240019d
Eduardo Segovia <eduardo.segovia@ucsp.edu.pe>
2023-03-24 14:08:34 -0500

    ejercicio 2

---
+++ practica1/ejercicio2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << std::fixed << std::setprecision(n) << M_PI << std::endl;
+    return 0;
+}
